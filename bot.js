1cf403fb4c3a91de7db145abf91ed70ec7226de1
MohabMohamed1 <41892765+MohabMohamed1@users.noreply.github.com>
2018-07-30 22:50:12 +0200

    Update bot.js

--- bot.js
+++ bot.js
@@ -893,16 +893,7 @@ client.on('message', function(message) {
 });
 
 
-	   client.on('message', message => {
-       if (message.content.startsWith(prefix + 'botserver')) {
-     let msg =  client.guilds.map(guild => `**${guild.name}** عدد الاعضاء: ${guild.memberCount}`).join('\n');
-  let embed = new Discord.RichEmbed()
-  .setTitle(`${client.guilds.size}سيرفرات `)
-  .setDescription(`${msg}`)
-  .setColor("#ebf442");
-  message.channel.send(embed);
-}
-});
+	
 
 
 
